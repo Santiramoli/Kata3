d0dedfa90013ff7d339ca69bbd2280cd0d69bc8b
SantiRamoli <sanolydani@gmail.com>
2023-11-22 23:56:56 +0000

    JFreeChartHistogramDisplay class created.

---
+++ src/main/java/software/ulpgc/kata3/MainFrame.java
@@ -0,0 +1,24 @@
+package software.ulpgc.kata3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private HistogramDisplay histogramDisplay;
+
+    public MainFrame() throws HeadlessException {
+        this.setTitle("Histogram viewer");
+        this.setSize(1100, 850);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(createHistogramDisplay());
+    }
+
+    private Component createHistogramDisplay() {
+        JFreeChartHistogramDisplay display = new JFreeChartHistogramDisplay();
+        this.histogramDisplay = display;
+        return display;
+    }
+
+    public HistogramDisplay histogramDisplay(){return histogramDisplay;}
+}
